30aa6fdb68044baf7253bcd00c0d465a64db0b83
EdgarJeremy <edgarpontoh3141@gmail.com>
2019-05-24 02:04:58 +0800

    (-) default message for InvalidRequestError

--- src/classes/InvalidRequestError.ts
+++ src/classes/InvalidRequestError.ts
@@ -3,5 +3,5 @@ import SiriusError from './SiriusError';
 export default class InvalidRequestError extends SiriusError {
 	name: string = 'InvalidRequestError';
 	code: number = 400;
-	message: string = 'InvalidRequest Error';
+	// message: string = 'InvalidRequest Error';
 }
